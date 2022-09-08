c7167ea2263a89cb9577a70776afe235c4628dca
daniscodes <dan97@bell.net>
2022-09-08 03:30:58 -0400

    Build interviewer component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,28 @@
+
+import React from "react";
+import cs from "classnames";
+
+import "components/InterviewerListItem.scss"
+
+export default function(props) {
+
+  const interviewerClass = cs(
+    "interviewers__item",{
+    "interviewers__item--selected": props.selected
+    }
+  );
+
+  return (
+    <li className={interviewerClass} onClick={() => {
+        props.setInterviewer(props.name);
+        }
+      }>
+      <img
+        className={"interviewers__item-image"}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  ); 
+}   
