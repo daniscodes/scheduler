67165d06dfe681f9395b046796a8537a7d04edb8
daniscodes <dan97@bell.net>
2022-09-10 03:12:16 -0400

    Retrieve interviewer data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
-import getAppointmentsForDay from "../helpers/selectors"
+import {getAppointmentsForDay, getInterview} from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    appointments: {}
-  });
 
+    appointments: {},
+    interviewers: {}
+  });
   let dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const setDay = day => {
@@ -28,21 +28,32 @@ export default function Application(props) {
   useEffect(()=>{
     const dayURL = "http://localhost:8001/api/days";
     const appointmentURL="http://localhost:8001/api/appointments";
-    //The below get request is for one API end point:
-    // axios.get(dayURL).then(response =>{
-    //   console.log(response.data)
-    //   setDays([...response.data]);
-    // })
+    const interviewersURL = "http://localhost:8001/api/interviewers";
+    
     Promise.all([
       axios.get(dayURL),
-      axios.get(appointmentURL)
+      axios.get(appointmentURL),
+      axios.get(interviewersURL)
+
     ]).then((all) =>{
-      // console.log("first promise resolved:",all[0]);
-      // console.log("second promise resolved:" ,all[1].data);
-      // console.log("all the promises:", all);
-      setState(prev=>({...prev, days:all[0].data, appointments:all[1].data}));
+      setState(prev=>({...prev, days:all[0].data, appointments:all[1].data, interviewers:all[2].data}));
     })
   },[]);
+
+  let appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map((appointment) => {
+  const interview = getInterview(state, appointment.interview);
+
+  return (
+    <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+    />
+  );
+});
 
   return (
     <main className="layout">
@@ -67,12 +78,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {dailyAppointments.map(appointment =>{
-          // return <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />
-          //If we want every key in an object to become a prop for a component, we can spread the object into the props definition
-          return <Appointment key={appointment.id} {...appointment} />
-        })}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,13 +1,28 @@
 export function getAppointmentsForDay(state, day) {
-  const selectedApptDay = state.days.filter(days => days.name === day);
-  const appointments = [];
+  const filteredDays = state.days.filter(day => day.name === name);
 
-  if (selectedApptDay[0]) {
-    const dayIDs = selectedApptDay[0].appointments
-
-    for (const numAppts of dayIDs) {
-      appointments.push(state.appointments[numAppts]);
-    }
+  if(state.days.length===0 || filteredDays.length===0){
+    return [];
   }
-  return appointments;
+
+  const appointmentsFromDays = filteredDays[0].appointments;
+
+  let filteredAppointments = [];
+
+  for(let appointment of appointmentsFromDays) {
+   
+    filteredAppointments.push(state.appointments[appointment]);
+
+
+  }
+  return filteredAppointments;
+}
+
+export  function getInterview(state, interview) {
+  if(!interview) return null;
+  const filteredInterview = {};
+  filteredInterview.student = interview.student;
+  filteredInterview.interviewer = state.interviewers[interview.interviewer];
+  return filteredInterview;
+
 }
